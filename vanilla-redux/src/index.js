a54688ba86b5bcb38cbaac647df8aa09ee765d5e
Blossssom <eju2003@gmail.com>
2022-04-19 01:00:44 +0900

    add basic code

---
+++ vanilla-redux/src/index.js
@@ -0,0 +1,27 @@
+import { createStore } from "redux";
+
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.getElementById("number");
+
+// const store = createStore();
+
+let count = 0;
+number.innerText = count;
+
+const updateText = () => {
+    number.innerText = count;
+};
+
+const handleAdd = () => {
+  count += 1;
+  updateText();
+};
+
+const handleMinus = () => {
+  count -= 1;
+  updateText();
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
