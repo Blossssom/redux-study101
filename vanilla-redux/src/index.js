1763faa1bdc8d08fc852afc2f0dfe3e28155b40c
Blosssom <eju2003@gmail.com>
2022-04-21 17:40:45 +0900

    make Todo list...

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,37 +1,39 @@
-import {createStore} from "redux"
+import { createStore } from "redux";
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.getElementById("number");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-const ADD = "ADD";
-const MINUS = "MINUS";
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
 
-const countModifier = (count = 0, action) => {
+const reducer = (state = [], action) => {
+  console.log(state, action)
   switch(action.type) {
-    case ADD:
-      return count + 1;
-    case MINUS:
-      return count - 1;
+    case ADD_TODO:
+      return [...state, {text: action.text, id: Date.now()}];
+    case DELETE_TODO:
+      return [];
     default:
-      return count;
+      return state;
   }
 };
 
-const countStore = createStore(countModifier);
-const onChange = () => {
-  number.innerText = countStore.getState();
+const store = createStore(reducer);
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+const addToDo = (text) => {
+  store.dispatch({type: ADD_TODO, text});
 };
 
-countStore.subscribe(onChange);
+const onSubmit = (e) => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  addToDo(toDo);
+}
 
-const handleAdd = () => {
-  countStore.dispatch({type: ADD});
-};
-
-const handleMinus = () => {
-  countStore.dispatch({type: MINUS});
-};
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+form.addEventListener("submit", onSubmit);
