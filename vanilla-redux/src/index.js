cf3ea87a660ab03b8c51f9c8baf02600abf6ae79
Blossssom <eju2003@gmail.com>
2022-04-22 23:39:18 +0900

    Complete delete and paint

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -7,11 +7,19 @@ const ul = document.querySelector("ul");
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
 
+const addTodo = (text) => {
+  return {type: ADD_TODO, text};
+}
+
+const deleteTodo = (id) => {
+  return {type: DELETE_TODO, id};
+};
+
 const reducer = (state = [], action) => {
   console.log(state, action)
   switch(action.type) {
     case ADD_TODO:
-      return [...state, {text: action.text, id: Date.now()}];
+      return [{text: action.text, id: Date.now()}, ...state];
     case DELETE_TODO:
       return [];
     default:
@@ -21,19 +29,41 @@ const reducer = (state = [], action) => {
 
 const store = createStore(reducer);
 
-store.subscribe(() => {
-  console.log(store.getState());
-});
-
-const addToDo = (text) => {
-  store.dispatch({type: ADD_TODO, text});
+const dispatchAddToDo = (text) => {
+  store.dispatch(addTodo(text));
 };
+
+const dispatchDeleteTodo = (e) => {
+  // 버튼이 li내부에 있으니 parentNode 사용
+  const id = e.target.parentNode.id;
+  store.dispatch(deleteTodo(id));
+};
+
+// paint? 단순 렌더링에 대한 내용인지 확인 필요
+// painting이 렌더링을 하며 새로 그리는 의미인듯 함
+const paintToDos = () => {
+  const toDos = store.getState();
+  ul.innerHTML = "";
+  toDos.forEach((toDo) => {
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.innerText = "DEL";
+    btn.addEventListener("click", dispatchDeleteTodo);
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    ul.appendChild(li);
+    li.appendChild(btn);
+  });
+};
+
+store.subscribe(paintToDos);
+
 
 const onSubmit = (e) => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
-  addToDo(toDo);
+  dispatchAddToDo(toDo);
 }
 
 form.addEventListener("submit", onSubmit);
